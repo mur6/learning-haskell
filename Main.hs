f34cfdcacb0227df533093067add1c6b72b50695
MURAKI Taichi <muraki.taichi@gmail.com>
2018-01-02 13:18:52 +0900

    modify

--- Main.hs
+++ Main.hs
@@ -1,20 +1,23 @@
 module Main where
 
--- import Data.Char
+import Data.Char
 -- import Data.Functor
 
 data Tree a = EmptyTree | Node a (Tree a) (Tree a)
   deriving (Show)
 
-singleton :: a -> Tree a
-singleton x = Node x EmptyTree EmptyTree
+sg :: a -> Tree a
+sg x = Node x EmptyTree EmptyTree
 
 treeInsert :: (Ord a) =>  a -> Tree a -> Tree a
-treeInsert x EmptyTree = singleton x
+treeInsert x EmptyTree = sg x
 treeInsert x (Node a left right)
   | x == a = Node a left right
   | x < a = Node a (treeInsert x left) right
   | otherwise = Node a left (treeInsert x right)
+
+threeNodeTree :: [a] -> Tree a
+threeNodeTree str = Node (str!!0) (sg $ str!!1) (sg $ str!!2)
 
 instance Functor Tree where
   fmap _ EmptyTree = EmptyTree
@@ -22,6 +25,11 @@ instance Functor Tree where
 
 main :: IO ()
 main = do
-  let nums = [8, 6, 4, 1, 7, 3, 5]::[Int]
-  let tree = foldr treeInsert EmptyTree nums
-  print $ fmap (*2) tree
+  -- let ll = Node 'L' (sg 'N') (sg 'T')
+  -- let yy = Node 'Y' (sg 'S') (sg 'A')
+  let t1 = Node 'O' (threeNodeTree "LNT") (threeNodeTree "YSA")
+  let t2 = Node 'L' (threeNodeTree "WCR") (threeNodeTree "AAC")
+  let freeTree = Node 'P' t1 t2
+  -- let nums = [8, 6, 4, 1, 7, 3, 5]::[Int]
+  -- let tree = foldr treeInsert EmptyTree nums
+  print $ fmap toLower freeTree
