afa52b844f41926260b01c8519713a5a6694e582
MURAKI Taichi <muraki.taichi@gmail.com>
2018-01-02 09:16:31 +0900

    just excercise.

--- Main.hs
+++ Main.hs
@@ -1,4 +1,16 @@
 module Main where
 
+class MyEq a where
+  equals :: a -> a -> Bool
+
+data MyValue = W Double deriving Show
+
+instance MyEq MyValue where
+  equals (W x) (W y) =  x == y
+
+t2 :: [Char]
+t2 = show $ W(2.2) `equals` W(2.2)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  putStrLn $ t2
