972f8490fc72725d4eb2004fc89d145fad955845
MURAKI Taichi <muraki.taichi@gmail.com>
2018-01-02 12:09:12 +0900

    learning learning `Type Class`.

--- Main.hs
+++ Main.hs
@@ -1,18 +1,27 @@
 module Main where
 
-class YesNo a where
-  yesno :: a -> Bool
+-- import Data.Char
+-- import Data.Functor
 
-data Tree a = EmptyTree | Node a deriving Show
+data Tree a = EmptyTree | Node a (Tree a) (Tree a)
+  deriving (Show)
 
-instance YesNo (Maybe a) where
-  yesno (Just _) = True
-  yesno Nothing = False
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
 
-instance YesNo (Tree a) where
-  yesno (Node _) = True
-  yesno EmptyTree = False
+treeInsert :: (Ord a) =>  a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x
+treeInsert x (Node a left right)
+  | x == a = Node a left right
+  | x < a = Node a (treeInsert x left) right
+  | otherwise = Node a left (treeInsert x right)
+
+instance Functor Tree where
+  fmap _ EmptyTree = EmptyTree
+  fmap f (Node x left right) = Node (f x) (fmap f left) (fmap f right)
 
 main :: IO ()
 main = do
-  print $ yesno Nothing
+  let nums = [8, 6, 4, 1, 7, 3, 5]::[Int]
+  let tree = foldr treeInsert EmptyTree nums
+  print $ fmap (*2) tree
