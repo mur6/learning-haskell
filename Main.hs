77da83fc4405e70e31caf873acac9926e76ba20e
MURAKI Taichi <muraki.taichi@gmail.com>
2018-01-02 10:14:12 +0900

    just excercise.

--- Main.hs
+++ Main.hs
@@ -11,6 +11,15 @@ instance MyEq MyValue where
 t2 :: [Char]
 t2 = show $ W(2.2) `equals` W(2.2)
 
+test3 :: IO ()
+test3 = do
+  _ <- putStrLn "Hello, what's your name?"
+  let tmp = getLine
+  name <- tmp
+  _ <- putStrLn $ "Zis is your future: " ++ name
+  return ()
+
 main :: IO ()
 main = do
-  putStrLn $ t2
+  -- putStrLn $ t2
+  test3
